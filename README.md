03c75d3b55a8bcca11f59251a7dda254b2761427
Corey Ferguson <corey.t.ferguson@gmail.com>
2015-10-26 19:12:04 -0700

    Git, grunt, and grunt bump.

--- README.md
+++ README.md
@@ -7,21 +7,33 @@ Yeoman generator for an express web application with require.js dependency manag
 
 Features:
 
-Technology                                       | Required
------------------------------------------------- | --------
-[express](https://github.com/strongloop/express) | Yes
-[requirejs](http://requirejs.org)                | Yes
-[bower](http://bower.io)                         | Yes
+Technology                                            | Required
+------------------------------------------------      | --------
+[express](https://github.com/strongloop/express)      | Yes
+[requirejs](http://requirejs.org)                     | Yes
+[bower](http://bower.io)                              | Yes
+[git](https://git-scm.com)                            | No
+[grunt](http://gruntjs.com/)                          | No
+[grunt-bump](https://github.com/vojtajina/grunt-bump) | No
 
-## Consumer Usage
+## Usage
 
 ```bash
 npm install -g generator-express-requirejs
+yo express-requirejs name-of-project
 ```
 
 ## Contributor Usage
 
 ```bash
+git clone git@github.com:coreyferguson/generator-express-requirejs.git
+cd generator-express-requirejs
 npm install
 grunt
+```
+
+To run generator with local build, link the generator to npm by executing this from the project directory: 
+
+```bash
+npm link
 ```
--- app/index.js
+++ app/index.js
@@ -4,28 +4,58 @@ module.exports = generators.Base.extend({
 
   constructor: function () {
     generators.Base.apply(this, arguments);
-    this.argument('appname', { type: String, required: true });
+    this.argument('appname', { type: String, required: false });
+  },
+
+  prompting: function() {
+    var done = this.async();
+    this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname
+      },
+      {
+        type: 'confirm',
+        name: 'isGitAvailable',
+        message: 'Configure git',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'isGruntAvailable',
+        message: 'Configure grunt',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'isGruntBumpAvailable',
+        message: 'Configure grunt-bump',
+        default: true,
+        when: function(answers) {
+          return answers.isGruntAvailable && answers.isGitAvailable;
+        }
+      }
+    ], function (answers) {
+      this.project = {
+        name: this.appname
+      };
+      this.environment = {
+        isGitAvailable: answers.isGitAvailable,
+        isGruntAvailable: answers.isGruntAvailable,
+        isGruntBumpAvailable: (answers.isGruntAvailable ? answers.isGruntBumpAvailable : false)
+      };
+      done();
+    }.bind(this));
   },
 
   configuring: function () {
     this.destinationRoot(this.appname);
     this.config.set('templateModel', {
-      project: {
-        name: this.appname
-      }
+      project: this.project,
+      environment: this.environment
     });
-  },
-
-  install: {
-    npm: function() {
-      this.npmInstall(['express'], { 'saveDev': true });
-    },
-    bowerRequireJs: function() {
-      this.npmInstall(['bower-requirejs'], { 'saveDev': true });
-    },
-    bower: function() {
-      this.bowerInstall('requirejs');
-    }
   },
 
   writing: {
@@ -38,6 +68,38 @@ module.exports = generators.Base.extend({
       this.fs.copyTpl(
         this.templatePath('writing/**/.*'),
         this.destinationPath());
+      if (this.environment.isGitAvailable) {
+        this.fs.copyTpl(
+          this.templatePath('git/**/.*'),
+          this.destinationPath());
+      }
+    }
+  },
+
+  install: {
+    npm: function() {
+      this.npmInstall(['express'], { 'saveDev': true });
+    },
+    bowerRequireJs: function() {
+      this.npmInstall(['bower-requirejs'], { 'saveDev': true });
+    },
+    bower: function() {
+      this.bowerInstall('requirejs');
+    },
+    grunt: function() {
+      // grunt
+      if (this.environment.isGruntAvailable) {
+        this.npmInstall('grunt');
+        this.gruntfile.insertConfig('pkg', 'grunt.file.readJSON("package.json")');
+
+        // grunt-bump
+        if (this.environment.isGruntBumpAvailable) {
+          this.npmInstall('grunt-bump');
+          this.gruntfile.insertConfig('bump', "{ options: { push: false } }");
+          this.gruntfile.loadNpmTasks('grunt-bump');
+        }
+
+      }
     }
   },
 
@@ -51,7 +113,19 @@ module.exports = generators.Base.extend({
       this.fs.copyTpl(
         this.templatePath('end/**/.*'),
         this.destinationPath());
-    }
+    },
+    git: function() {
+      if (this.environment.isGitAvailable) {
+        var that = this;
+        that.spawnCommand('git', ['init'])
+        .on('close', function(code) {
+          that.spawnCommand('git', ['add', '--all'])
+          .on('close', function(code) {
+            that.spawnCommand('git', ['commit', '-m', 'init']);
+          });
+        });
+      }
+    },
   }
   
 });
