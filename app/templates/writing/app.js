f15457b83c06e404a53ad193a11f1c14121d7dda
Corey Ferguson <corey.t.ferguson@gmail.com>
2015-10-26 18:23:43 -0700

    Bower implementation fixes.

--- app/templates/writing/app.js
+++ app/templates/writing/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.use('/lib', express.static('bower_components'));
+app.use('/bower_components', express.static('bower_components'));
 
 var server = app.listen(3000, function () {
 	console.log('Server running on port 3000');
